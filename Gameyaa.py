4c88da5ebab409778fede590e7cc74e9e8a9f3e1
Mahmoud Hussain <121444044+Mahmoud-Hussain@users.noreply.github.com>
2024-01-05 15:51:54 +0600

    a basic update
    a basic update off the icon of the window and window caption

--- Gameyaa.py
+++ Gameyaa.py
@@ -1,8 +1,18 @@
 import pygame
 
 pygame.init()
-SCREEN = pygame.display.set_mode((640,400))
-    
+SCREEN = pygame.display.set_mode((800,600))
+
+#Adding the Logo on the window
+logo_image = pygame.image.load('car.png')
+pygame.display.set_icon(logo_image)
+
+#displaying the name of the window 
+pygame.display.set_caption("Gameyaa")
+
+#creating a player
+player = pygame.rect((350,250,50,50))
+
 run = True
 while run:
     for event in pygame.event.get():
