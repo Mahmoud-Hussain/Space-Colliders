b73a4f1cf558bb2ea317eeb10dc9ba0f9da50c43
Mahmoud Hussain <121444044+Mahmoud-Hussain@users.noreply.github.com>
2024-01-10 15:17:54 +0600

    Updated background
    different backgrounds have been added to the game, each time the game runs

--- Gameyaa.py
+++ Gameyaa.py
@@ -1,11 +1,12 @@
 import pygame
 import os
+import random
 
 # initiallize pygame's library
 pygame.init() 
 
-HEIGHT , WIDTH = 800,600
-SCREEN = pygame.display.set_mode((HEIGHT,WIDTH))
+WIDTH, HEIGHT= 800,600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Adding the Logo on the window
 # logo_image = pygame.image.load()
@@ -15,59 +16,66 @@ SCREEN = pygame.display.set_mode((HEIGHT,WIDTH))
 pygame.display.set_caption("Space colliders")
 
 FPS = 60
-
+SPACESHIP_VEL = 7
 WHITE = (255,255,255)
+BACKGROUND_IMAGES = ['background-01.jpg', 'backgound2-01.jpg', 'background3.jpg', 'background4.jpg']
+rand = random.randint(0,3)
 
 SpaceShip_Width , SpaceShip_Height = 55, 60 
 
 Yellow_Spaceship_image = pygame.image.load(
     os.path.join('Assets','yellow_spaceship.png' )) 
-Yellow_SpaceShip = pygame.transform.rotate(pygame.transform.scale(Yellow_Spaceship_image, (SpaceShip_Width,SpaceShip_Height)),270)
+Yellow_SpaceShip = pygame.transform.rotate(
+    pygame.transform.scale(Yellow_Spaceship_image, (SpaceShip_Width,SpaceShip_Height)),270)
 
 Red_Spaceship_image = pygame.image.load(
     os.path.join('Assets','red_spaceship.png' )) 
-Red_SpaceShip = pygame.transform.rotate(pygame.transform.scale(Red_Spaceship_image, (SpaceShip_Width,SpaceShip_Height)),90)
+Red_SpaceShip = pygame.transform.rotate(
+    pygame.transform.scale(Red_Spaceship_image, (SpaceShip_Width,SpaceShip_Height)),90)
 
 Background_Image = pygame.image.load(
-    os.path.join('Assets','background-01.jpg'))
-Background_Image_scale = pygame.transform.scale(Background_Image, (HEIGHT,WIDTH))
+    os.path.join('Assets', BACKGROUND_IMAGES[rand]))
+Background_Image_scale = pygame.transform.scale(Background_Image, (WIDTH, HEIGHT))
 
-yellow = pygame.Rect(145, 100, SpaceShip_Width, SpaceShip_Height)
-red = pygame.Rect(545, 100, SpaceShip_Width, SpaceShip_Height)
+
 
     
-def draw_Window():
+def draw_Window(red,yellow):
+    
     SCREEN.fill(WHITE)
     SCREEN.blit(Background_Image_scale, (0,0))
     SCREEN.blit(Yellow_SpaceShip, (yellow.x, yellow.y))
     SCREEN.blit(Red_SpaceShip, (red.x, red.y))
     pygame.display.update()
 
-def movement_yellow():
+def movement_yellow(red,yellow):
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        yellow.x -= 5  
-    if keys[pygame.K_RIGHT]:
-        yellow.x += 5  
-    if keys[pygame.K_UP]:
-        yellow.y -= 5 
-    if keys[pygame.K_DOWN]:
-        yellow.y += 5  
+    if keys[pygame.K_LEFT] and yellow.x - SPACESHIP_VEL > 0:
+        yellow.x -= SPACESHIP_VEL  
+    if keys[pygame.K_RIGHT] and yellow.x < WIDTH/2 - SpaceShip_Width - 10:
+        yellow.x += SPACESHIP_VEL  
+    if keys[pygame.K_UP] and yellow.y - SPACESHIP_VEL > 0:
+        yellow.y -= SPACESHIP_VEL 
+    if keys[pygame.K_DOWN] and yellow.y < HEIGHT - SpaceShip_Height:
+        yellow.y += SPACESHIP_VEL  
 
-def movement_red():
+def movement_red(red,yellow):
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        red.x -= 5  
-    if keys[pygame.K_d]:
-        red.x += 5  
-    if keys[pygame.K_w]:
-        red.y -= 5 
-    if keys[pygame.K_s]:
-        red.y += 5  
+    if keys[pygame.K_a] and red.x > WIDTH/2 + 10:
+        red.x -= SPACESHIP_VEL  
+    if keys[pygame.K_d] and red.x < WIDTH - SpaceShip_Width - 8:
+        red.x += SPACESHIP_VEL  
+    if keys[pygame.K_w] and red.y - SPACESHIP_VEL > 0:
+        red.y -= SPACESHIP_VEL 
+    if keys[pygame.K_s] and red.y < HEIGHT - SpaceShip_Height:
+        red.y += SPACESHIP_VEL  
 
 
 
 def main():
+    print(rand)
+    yellow = pygame.Rect(145, 100, SpaceShip_Width, SpaceShip_Height)
+    red = pygame.Rect(545, 100, SpaceShip_Width, SpaceShip_Height)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -77,9 +85,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
         
-        draw_Window()
-        movement_yellow()  
-        movement_red()   
+        draw_Window(red,yellow)
+        movement_yellow(red,yellow)  
+        movement_red(red,yellow)   
     pygame.quit()
     
 if __name__ == "__main__":
